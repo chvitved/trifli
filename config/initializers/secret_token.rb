99023b42e0cbf88277d4f5b051726b6f59ab73b0
Thomas Børlum <thb@trifork.com>
2011-01-19 10:26:42 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trifli::Application.config.secret_token = '70032100468e1a151422efdffca7020b7424ae34b84f588fff09cde333ccfc0b480fff82ffebc300eb9a57e4590187c513eaf80c25dd4c779747bb412c1b2e19'
